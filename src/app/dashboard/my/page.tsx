1d216a3acc4cc171af3e409e273eb7959b8d2120
Louis <836250617@qq.com>
2023-08-12 19:00:07 +0800

    finish the first ver of the 'my' page

--- src/app/dashboard/my/page.tsx
+++ src/app/dashboard/my/page.tsx
@@ -1,5 +1,37 @@
+import Link from "next/link";
+
 export default function MyPage() {
     return (
-        <h1 className="">My</h1>
+        <>
+            <div className="flex flex-col h-screen">
+                <div className="bg-white shadow-md p-4">
+                    <h2 className="text-lg font-semibold mb-4">待办会议</h2>
+                    <ul>
+                        <li className="flex items-center space-x-2 mb-2">
+                            <input type="checkbox" className="form-checkbox" />
+                            <span>Meeting 1</span>
+                        </li>
+                        <li className="flex items-center space-x-2 mb-2">
+                            <input type="checkbox" className="form-checkbox" />
+                            <span>Meeting 2</span>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="bg-white mt-4 shadow-md p-4">
+                    <h2 className="text-lg font-semibold mb-4">历史会议</h2>
+                    <ul>
+                        <li className="flex items-center mb-2 justify-between">
+                            <span className="">Meeting A - Completed</span>
+                            <Link className="" href="/detail">详情</Link>
+                        </li>
+                        <li className="flex items-center mb-2 justify-between">
+                            <span>Meeting B - Completed</span>
+                            <Link className="" href="/detail">详情</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
     )
 }
--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -5,20 +5,21 @@ import { options as authOptions } from "../../api/auth/[...nextauth]/options"
 export default async function ProfilePage() {
     const session = await getServerSession(authOptions);
     const name = session?.user?.name;
+    const email = session?.user?.email;
+    const image = session?.user?.image;
     return (
         <>
-            <div className="flex flex-col items-center justify-center min-h-screen">
-                <div className="bg-white rounded-lg shadow-md p-8 max-w-md">
-                    <div className="mb-4">
-                    </div>
+            <div className="flex flex-col items-center min-h-screen">
+                <div className="bg-white rounded-lg shadow-md p-8 max-w-md min-w-full">
+                    <div className="mb-4"></div>
                     <h1 className="text-gray-600 text-2xl font-semibold mb-2">{name ? name : "unknown"}</h1>
-                    <p className="text-gray-500 mb-4">UI/UX Designer</p>
+                    <p className="text-gray-500 mb-4">{email ? email : "unknown"}</p>
                     <div className="border-t border-gray-300 pt-4">
-                        <p className="text-sm text-gray-700">Bio: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p className="text-sm text-gray-700">Some introduce ...... </p>
                     </div>
                 </div>
                 <div className="text-white text-center py-4">
-                    <Link className="px-4 py-2 border shadow-sm rounded-sm text-black bg-gray-100 hover:bg-gray-50" href="/api/auth/signout">Sign out</Link>
+                    <Link className="px-4 py-2 rounded-lg border shadow-sm text-black bg-gray-100 hover:bg-gray-50" href="/api/auth/signout">Sign out</Link>
                 </div>
             </div>
         </>
