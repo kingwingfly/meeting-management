7e06a960b8451b000756a99c46fd1c37a73d31c4
Louis <836250617@qq.com>
2023-08-21 08:55:22 +0800

    ready to dev wasm

--- src/app/api/create-meeting/route.ts
+++ src/app/api/create-meeting/route.ts
@@ -2,5 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
     let meeting = await req.json();
+    // todo use wasm to impl meeting arrangement
     return NextResponse.json({ "result": "good" })
 }
