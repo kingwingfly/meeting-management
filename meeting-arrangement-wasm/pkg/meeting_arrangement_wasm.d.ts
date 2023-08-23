08a900f58c54bfcbcd0bba0aa31579385746bf78
Louis <836250617@qq.com>
2023-08-23 17:49:22 +0800

    add wasm into git trace

---
+++ meeting-arrangement-wasm/pkg/meeting_arrangement_wasm.d.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @returns {any}
+*/
+export function newOcs(): any;
+/**
+* @param {any} ocs
+* @param {number} duration
+* @returns {Uint32Array}
+*/
+export function arrange(ocs: any, duration: number): Uint32Array;
+/**
+* @returns {any}
+*/
+export function newGenSqlData(): any;
+/**
+* @param {any} create_data
+* @returns {any}
+*/
+export function genSqls(create_data: any): any;
---
+++ meeting-arrangement-wasm/pkg/meeting_arrangement_wasm.js
@@ -0,0 +1,4 @@
+import * as wasm from "./meeting_arrangement_wasm_bg.wasm";
+import { __wbg_set_wasm } from "./meeting_arrangement_wasm_bg.js";
+__wbg_set_wasm(wasm);
+export * from "./meeting_arrangement_wasm_bg.js";
