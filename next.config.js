d20fc93280c792bbe2e92a1ebe8bfce37e42f60f
Louis <836250617@qq.com>
2023-08-24 02:35:52 +0800

    modified: next.config.js for wasm deploy

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    webpack(config, { isServer, dev }) {
+  webpack(config, { isServer, dev }) {
     // Use the client static directory in the server bundle and prod mode
     // Fixes `Error occurred prerendering page "/"`
     config.output.webassemblyModuleFilename =
       isServer && !dev
-        ? '../static/wasm/[modulehash].wasm'
-        : 'static/wasm/[modulehash].wasm'
+        ? '../../meeting-arrangement-wasm/pkg/meeting_arrangement_wasm_bg.wasm'
+        : '../meeting-arrangement-wasm/pkg/meeting_arrangement_wasm_bg.wasm'
 
     // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
     config.experiments = { ...config.experiments, asyncWebAssembly: true }
